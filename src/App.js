d8a5a6eb471a9bd4e9c1d6f26ba6bc8619ab6140
Luiz Freitas <luiz_dev@outlook.com>
2025-06-10 11:31:30 -0300

    [ADD] - vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 import JobOffers from './components/JobOffers/JobOffers';
 import Presentation from './components/Presentation/Presentation';
@@ -60,6 +61,7 @@ function App() {
         <JobOffers jobOffers={jobOffers} />
         <Contacts contacts={contacts} />
         <Location location={location} />
+        <Analytics />
       </div>
     </CSSTransition>
   );
